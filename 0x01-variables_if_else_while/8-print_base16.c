76328a1914e18e6dd52f1d424706248bedc9dd41
A-jay <you@example.com>
2023-07-14 18:10:45 -0700

    1

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: goes here
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 48;
+
+	while (x <= 102)
+
+	{
+	putchar(x);
+
+		if (x == 57)
+			X += 37;
+			x++
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
