419b926c59570a14366b9a32d2799c9051fd720d
A-jay <you@example.com>
2023-07-14 17:32:06 -0700

    1

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: goes here
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		putchar(x + '0');
+		x++;
+	}
+	putchar("\n");
+
+	return (0);
+}
