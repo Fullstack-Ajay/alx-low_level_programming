a4ad891d917ec843912400afa46806555f375e19
A-jay <you@example.com>
2023-07-20 18:12:18 -0700

    1

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,13 +15,16 @@ void print_diagonal(int n)
 	else
 
 	{
-		for (x = 1; x <= n; x++)
+		for (x = 0; x <= n; x++)
 
 		{
-			for (y = 1; y <= x; y++)
-				_putchar(' ');
-
-			_putchar(92);
+			for (y = 0; y <= n; y++)
+			{
+				if (y == x)
+					_putchar('\\');
+				else if (y < x)
+					_putchar(' ');
+			}
 			_putchar('\n');
 
 
