b2f99a53b704321ea9bb3ddeee63e6694d3b88a9
A-jay <you@example.com>
2023-07-20 17:15:56 -0700

    1

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print_numbers - prints numbers from 0 to 9 excluding 2 and 4
+ * print_most_numbers - prints numbers from 0 to 9 excluding 2 and 4
  *
  * Return: Always 0 (success)
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
