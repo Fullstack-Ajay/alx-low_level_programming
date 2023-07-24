440ee641d7f355ca73518d0db6453f338af805a4
A-jay <you@example.com>
2023-07-24 14:34:53 -0700

    1

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - prints half string
+ *
+ * @str: input
+ *
+ *
+ * Return: none
+ */
+void puts2(char *str)
+
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		if (x % 2 == 0)
+		_putchar(str[x]);
+	}
+	_putchar('\n');
+}
