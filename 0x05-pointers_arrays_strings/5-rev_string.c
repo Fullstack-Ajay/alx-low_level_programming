75bcee5d04d12cccec054b9a64705ce524ba91a1
A-jay <you@example.com>
2023-07-24 13:49:18 -0700

    1

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,8 +16,8 @@ void rev_string(char *s)
 		for (i = 0; i < (l / 2); i++)
 		{
 			temp = s[i];
-			s[i] = s[l - 1 - i];
-			s[l - 1 - i] = temp;
+			s[i] = s[l - i - 1];
+			s[l - i - 1] = temp;
 		}
 }
 
