99868ea691ac8f2bc107be367129ba3f37b5e75d
A-jay <you@example.com>
2023-08-05 07:37:39 -0700

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncat - manipulates strings
+ *
+ * @dest: input
+ * @src: input
+ * @n: input
+ *
+ * Return: character
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int c, i;
+
+	c = 0;
+
+	while (dest[c])
+		c++;
+
+	for (i = 0; i < n && src[n] != '\0'; i++)
+		dest[c + i] = src[i];
+	dest[c + i] = '\0';
+
+	return (dest);
+}
